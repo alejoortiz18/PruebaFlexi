c65bd6850f8dffbfb6d639ebba17cefa6473551f
alejoortiz18 <45045371+alejoortiz18@users.noreply.github.com>
2022-02-11 01:56:02 -0500

    Se crea la clase Departamento

--- EjercicioCiclismo/MiddleWare/WebApi/WebApi/Controllers/DepartamentoController.cs
+++ EjercicioCiclismo/MiddleWare/WebApi/WebApi/Controllers/DepartamentoController.cs
@@ -3,38 +3,37 @@ using Entities;
 using System;
 using System.Collections.Generic;
 using System.Web.Http;
+using rto = System.Web.Http;
 
 namespace WebApi.Controllers
 {
-    // public class ValuesController : ApiController  ValuesController
-    public class ProductoController : ApiController
+    public class DepartamentoController : ApiController
     {
-
         private ProductoBL producto = new ProductoBL();
 
 
-        [Route("Producto/GetList")]
+        [rto.Route("Departamento/GetList")]
         public List<ProductoE> GetList()
         {
             // List<ProductoE> result = producto.GetList();
             return producto.GetList();
         }
 
-        [Route("Producto/Insert")]
+        [rto.Route("Departamento/Insert")]
         public Tuple<bool, string> Insert(ProductoE prod)
         {
             Tuple<bool, string> result = producto.Insert(prod);
             return result;
         }
 
-        [Route("Producto/Update")]
+        [rto.Route("Departamento/Update")]
         public Tuple<bool, string> Update(ProductoE prod)
         {
             Tuple<bool, string> result = producto.Update(prod);
             return result;
         }
 
-        [Route("Producto/Delete")]
+        [rto.Route("Departamento/Delete")]
         public Tuple<bool, string> Delete(int productoid)
         {
             Tuple<bool, string> result = producto.Delete(productoid);
